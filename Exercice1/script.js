d7caf5066184a67efc21e1ed280fb94537f01dc8
fiacre <armeldion95@gmail.com>
2023-01-25 22:34:17 +0000

    first commit

---
+++ Exercice1/script.js
@@ -0,0 +1,12 @@
+let VariableOptions = document.getElementsByName("option");
+
+for(let Option in VariableOptions ){
+
+    if(Option == 0){
+        console.log(VariableOptions[Option]);
+    }
+    else{
+        console.log(VariableOptions[Option]);
+    }
+
+}
